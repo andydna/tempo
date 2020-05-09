cc1b1e26bdc6b749e12e8e650fa3b0094551cebc
andydna <andydna@protonmail.ch>
2020-05-08 21:03:57 -0500

    cleaning

--- tempo.rb
+++ tempo.rb
@@ -21,18 +21,6 @@ class Tempo
     @taps = []
   end
 
-  def report
-    puts "\n#{bpm.round(2)} taps per minute"
-    puts
-    puts "4/4 downbeats?\t#{bpm.round(2)*4} bpm"
-    puts "3/4 downbeats?\t#{bpm.round(2)*3} bpm"
-    puts  "backbeats?\t#{(bpm*2).round(2) } bpm"
-    puts  "quarters?\t#{bpm.round(2)      } bpm"
-    puts  "eighths?\t#{(bpm/2).round(2)   } bpm"
-    puts  "triplets?\t#{(bpm/3).round(2)  } bpm"
-    puts  "sixteenths?\t#{(bpm/4).round(2)} bpm"
-  end
-
   def run
     hello
     loop do
@@ -43,10 +31,32 @@ class Tempo
     self
   end
 
+  def report
+    puts "\n#{bpm.round(2)} taps per minute"
+    puts
+    puts "4/4 downbeats?\t#{bpm.round(2)*4 } bpm"
+    puts "3/4 downbeats?\t#{bpm.round(2)*3 } bpm"
+    puts "backbeats?\t#{   (bpm*2).round(2)} bpm"
+    puts "quarters?\t#{     bpm.round(2)   } bpm"
+    puts "eighths?\t#{     (bpm/2).round(2)} bpm"
+    puts "triplets?\t#{    (bpm/3).round(2)} bpm"
+    puts "sixteenths?\t#{  (bpm/4).round(2)} bpm"
+  end
+
   def hello
     puts "tap almost any key for tempo"
     puts "press return, escape, or tab to finish"
     puts "you may fire when ready"
+  end
+
+  def bpm
+    begin
+    60 / (diffs.sum / diffs.count)
+    rescue ZeroDivisionError
+      puts "\n\tneed at least two taps\n\n"
+      reset
+      run
+    end
   end
 
   def diffs
@@ -58,16 +68,6 @@ class Tempo
   def reset
     @taps.clear
     @diffs = nil
-  end
-
-  def bpm
-    begin
-    60 / (diffs.sum / diffs.count)
-    rescue ZeroDivisionError
-      puts "\n\tneed at least two taps\n\n"
-      reset
-      run
-    end
   end
 end
 
